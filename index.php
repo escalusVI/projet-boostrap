b8d05ce1669923685fb7697ede6c9addb4b6a355
escalusVI <loicvaleryvi@gmail.com>
2017-01-05 20:35:04 +0100

    ajout footer

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="styleIndex.css">
+		<link rel="stylesheet" href="include/styleFooter.css"/>
 		<title>Abder Auto vente de vehicules</title>
 	</head>
 	<body>
