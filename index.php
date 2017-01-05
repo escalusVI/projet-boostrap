0d6a24554e1871da47ce44c916745576d1851c0d
escalusVI <loicvaleryvi@gmail.com>
2017-01-05 14:26:18 +0100

    creation css et ajout fichier header et footer

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="styleIndex.css">
+		<title>Abder Auto vente de vehicules</title>
+	</head>
+	<body>
+	
+		<header>
+			<?php include ("include/header.php");?>
+		</header>
+	
+		<footer>
+			<?php include ("include/footer.php");?>
+		</footer>
+	</body>
+	
+</html>
