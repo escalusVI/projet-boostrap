0ce4d36a599487033d2efc9f1e8aea63f26bbff3
escalusVI <loicvaleryvi@gmail.com>
2017-01-09 11:19:30 +0100

    ajout de sprite avec animation pour les resaux sociaux

--- include/footer.php
+++ include/footer.php
@@ -1,17 +1,21 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-4 social" >
-			<a href="#" id="fb"><img src="images/facebook.png"/></a>
+		<div class="col-md-4 sm-6 social" >
+			<!--<a href="#" id="fb"><img src="images/facebook.png"/></a>
 			<a href="#" id="tw"><img src="images/twitter.png"/</a>
-			<a href="#" id="pt"><img src="images/pinterest.png"/</a>
+			<a href="#" id="pt"><img src="images/pinterest.png"/</a>-->
+			<a href="#" id="fb" class="icon"></a>
+			<a href="#" id="tw" class="icon"></a>
+			<a href="#" id="pt" class="icon"></a>
+
 				<div class="col-md-12" id="cgv">
 					<a href="#"> CGV et mention legal</a>
 				</div>
 		</div>
-		<div class="col-md-4 locate">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2594.457863632722!2d1.0928295156949668!3d49.43806267934849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e0de790e570611%3A0x4988fb8e78330ff2!2sLa+Halle+Aux+Toiles!5e0!3m2!1sfr!2sfr!4v1483629397729" width="200" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<div class="col-md-4 sm-0 locate">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2594.457863632722!2d1.0928295156949668!3d49.43806267934849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e0de790e570611%3A0x4988fb8e78330ff2!2sLa+Halle+Aux+Toiles!5e0!3m2!1sfr!2sfr!4v1483629397729" width="400" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
-		<div class="col-md-4 address" >
+		<div class="col-md-4 sm-6 address" >
 			<h1>Abder Auto</h1><p>la halle aux toiles place de la basse vieille tour</br> 76000 rouen</p>
 		</div>
 </div>
