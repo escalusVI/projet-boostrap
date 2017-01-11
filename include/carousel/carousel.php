7036aa7a8e1620850ea9efeddba12539397bbe27
escalusVI <loicvaleryvi@gmail.com>
2017-01-11 22:11:31 +0100

    mise en page de la section et ajout du carousel

---
+++ include/carousel/carousel.php
@@ -0,0 +1,40 @@
+<div class="container">
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  
+        <!-- Indicators -->
+        <ol class="left carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+          
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=40&bg=4C4028&txtclr=ffffff&txt=SLIDE 1&w=1170&h=300" alt="...">
+            </div>
+        
+            <div class="item">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=40&bg=4C4028&txtclr=ffffff&txt=SLIDE 2&w=1170&h=300" alt="...">
+            </div>
+        
+            <div class="item">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=40&bg=4C4028&txtclr=ffffff&txt=SLIDE 3&w=1170&h=300" alt="...">
+            </div>
+        </div>
+        
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+  
+    </div>
+
+</div>
+
+<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -14,40 +14,42 @@
 		<header>
 			<?php include ("include/header.php");?>
 		</header>
+			
+			<?php include ("include/carousel/carousel.php");?>
 		
 		<section>
 			<article>
 			
 				<div class="row">
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
@@ -58,35 +60,35 @@
 			<article>
 			
 				<div class="row">
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
@@ -97,35 +99,35 @@
 			<article>
 			
 				<div class="row">
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
@@ -136,35 +138,35 @@
 			<article>
 			
 				<div class="row">
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
@@ -175,35 +177,35 @@
 			<article>
 			
 				<div class="row">
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
 				  
-				  <div class="col-md-4">
+				  <div class="col-md-4 hidden-sm hidden-xs ">
 					<div class="thumbnail">
 					  <img src="images/abder_auto.png" alt="...">
 					  <div class="caption">
 						<h3>Modele</h3>
 						<p>Descriptif</p>
-						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default" role="button">En savoir plus</a></p>
+						<p><a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> <a href="#" class="btn btn-default pull-right" role="button">En savoir plus</a></p>
 					  </div>
 					</div>
 				  </div>
@@ -216,6 +218,8 @@
 		<footer>
 			<?php include ("include/footer.php");?>
 		</footer>
+		
+
 	</body>
 	
 </html>
