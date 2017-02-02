739b8f57bbec4774655c872a2ac370c993078f90
abderrahmanerabhi <abder55@gmail.com>
2017-02-02 17:22:57 +0100

    modification du header(revoir la mise en page

--- include/header.php
+++ include/header.php
@@ -1,18 +1,22 @@
-<div class="container" id="header">
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-1 col-md-offset-11 connection">
+           <a href="connection.php"> connection</a>
+        </div>
+    </div>
     <div class="row">
-        <div class="col-md-2 logo">
-            <img src="images/abder_auto.png">
-            </div>
-        <div class="col-md-8 text-center title">
-            <h1>Le site auto de Abder</h1>
-            </div>
-        <div class="col-md-2 info ">
-            <div class="connect ">
-                partie connection
-                </div>
-                <div class="search ">
-                    partie recherche
-                    </div>
-            </div>
+       <div class="col-md-2 logo"><img src="images/abder_auto.png"></div>
+       <div class="col-md-offset-2 col-md-4 text-center titre"><h1>Abder Auto</h1></div>     
+    </div>
+    <div class="row"> 
+    <div class="col-md-offset-9 col-md-3 pull-right recherche">
+        <div class="input-group custom-search-form">
+              <input type="text" class="form-control">
+              <span class="input-group-btn">
+              <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+             </button>
+             </span>
+             </div><!-- /input-group --></div>
     </div>
 </div>
