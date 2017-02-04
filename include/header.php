2e9fb08f10e7290855e2bb408bcaf893fbd74947
abderrahmanerabhi <abder55@gmail.com>
2017-02-05 00:25:34 +0100

    ajout de roue a l'animation,amelioration. et travail(hypothese) sur xml

--- include/header.php
+++ include/header.php
@@ -6,8 +6,17 @@
     </div>
     <div class="row text-center">
        <div class="col-md-2 logo"><img src="images/abder_auto.png"></div>
-       <div class="col-md-offset-3 col-md-2 text-center anim"><h1>Abder Auto</h1></div>     
-    </div>
+       <div class="col-md-offset-3 col-md-2 text-center anim">
+        <h1>Abder Auto</h1>  
+        <div class="row roue">
+                <div class="col-md-offset-1 col-md-1 rouegauche"><img src="images/pneu.jpg">
+                </div>
+                <div class="col-md-offset-5 col-md-1 rouedroite"><img src="images/pneu.jpg">
+                </div>
+        </div>   
+       
+     </div>
+
     <div class="row"> 
     <div class="col-md-offset-9 col-md-3 pull-right recherche">
         <div class="input-group input-group-sm custom-search-form">
@@ -18,5 +27,5 @@
              </button>
              </span>
              </div><!-- /input-group --></div>
-    </div>
-</div>
+         </div>
+     </div>
